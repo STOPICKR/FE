65ef4dabcba3f4607bac97ee820c0e23574db32d
YunJae00 <0109548@naver.com>
2024-11-04 17:16:07 +0900

    navSection navigate 버튼 추가 및 이미지 제거

--- src/screen/pc/pages/main/section/NavSection.jsx
+++ src/screen/pc/pages/main/section/NavSection.jsx
@@ -1,46 +1,33 @@
 import React from "react";
 import styled from "styled-components";
-import stock_character from "../../../../../images/stock_character.png"
+import { useNavigate } from "react-router-dom";
 import {useMediaQuery} from "react-responsive";
 
-const wrapTextWithLang = (text) => {
-    return text.split('\n').map((line, index) => (
-        <React.Fragment key={index}>
-            {line.split('').map((char, index) => {
-                const isKorean = /[가-힣]/.test(char);
-                const lang = isKorean ? 'ko' : 'en';
-                return <span key={index} lang={lang}>{char}</span>;
-            })}
-            <br/>
-        </React.Fragment>
-    ));
-};
-
 const NavSectionWrapper = styled.div`
-    padding: 0 1em;
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
 `;
 
 const NavSectionContainer = styled.div`
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
-    padding: 5em 0;
+    padding: 5em 0 ;
     width: 100%;
-    gap: 3em;
+    gap: 0.5em;
 `;
 
 const MobileNavSectionContainer = styled.div`
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
     padding: 3.5em 0;
     width: 100%;
-    gap: 2em;
+    gap: 0.2em;
 `;
 
 const NavTitle = styled.p`
@@ -48,12 +35,8 @@ const NavTitle = styled.p`
     white-space: pre-line;
     font-weight: bold;
     line-height: 1.2;
-    [lang="en"] {
-        font-family: 'Inter', sans-serif;
-    }
-    [lang="ko"] {
-        font-family: 'pretendard', sans-serif;
-    }
+    text-align: center;
+    font-family: 'pretendard', sans-serif;
 `;
 
 const MobileNavTitle = styled.p`
@@ -61,36 +44,24 @@ const MobileNavTitle = styled.p`
     white-space: pre-line;
     font-weight: bold;
     line-height: 1.2;
-    [lang="en"] {
-        font-family: 'Inter', sans-serif;
-    }
-    [lang="ko"] {
-        font-family: 'pretendard', sans-serif;
-    }
+    text-align: center;
+    font-family: 'pretendard', sans-serif;
 `;
 
 const NavContent = styled.p`
     font-size: 1.25rem;
     white-space: pre-line;
     line-height: 1.2;
-    [lang="en"] {
-        font-family: 'Inter', sans-serif;
-    }
-    [lang="ko"] {
-        font-family: 'pretendard', sans-serif;
-    }
+    text-align: center;
+    font-family: 'pretendard', sans-serif;
 `;
 
 const MobileNavContent = styled.p`
     font-size: 0.8em;
     white-space: pre-line;
     line-height: 1.4;
-    [lang="en"] {
-        font-family: 'Inter', sans-serif;
-    }
-    [lang="ko"] {
-        font-family: 'pretendard', sans-serif;
-    }
+    text-align: center;
+    font-family: 'pretendard', sans-serif;
 `;
 
 const LogoImage = styled.img`
@@ -105,7 +76,36 @@ const MobileLogoImage = styled.img`
     right: 2.5em;
 `;
 
+const Button = styled.button`
+    padding: 12px 0;
+    margin-top: 2em;
+    font-size: 16px;
+    width: 30%;
+    background-color: #264653;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-weight: bold;
+`;
+
+const MobileButton = styled.button`
+    padding: 0.6em 0;
+    margin-top: 1em;
+    font-size: 0.8em;
+    width: 30%;
+    background-color: #264653;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-weight: bold;
+    font-family: 'pretendard', sans-serif;
+`;
+
 const NavSection = () => {
+
+    const navigate = useNavigate();
 
     const isOverTablet = useMediaQuery({
         query: "(min-width:720px)"
@@ -114,29 +114,33 @@ const NavSection = () => {
         query: "(max-width:720px)"
     });
 
+    const navigateStockList = () => {
+        navigate("/stock-list");
+    };
+
     return (
         <>
             {isOverTablet &&
                 <NavSectionWrapper>
                     <NavSectionContainer>
-                        <div>
-                            <NavTitle style={{color: "#1454FF"}}>{wrapTextWithLang("어려운 주식 투자")}</NavTitle>
-                            <NavTitle style={{paddingBottom: "1rem"}}>{wrapTextWithLang("AI 기술과 함께")}</NavTitle>
-                            <NavContent>{wrapTextWithLang("AI 모델 성과 및 수익률 분석\n서비스에서 선정된 산별 현황을 확인해보세요")}</NavContent>
-                        </div>
-                        <LogoImage src={stock_character} alt={"image"}/>
+                            <NavTitle style={{color: "#1454FF"}}>{"어려운 주식 투자"}</NavTitle>
+                            <NavTitle style={{paddingBottom: "1rem"}}>{"AI 기술과 함께 알아보세요"}</NavTitle>
+                            <NavContent>{"AI 모델 성과 및 수익률 분석\n서비스에서 선정된 산별 현황을 확인해보세요"}</NavContent>
+                            <Button onClick={navigateStockList}>AI 성과 분석 확인하기</Button>
+                        {/*<LogoImage src={Logo} alt={"image"}/>*/}
                     </NavSectionContainer>
                 </NavSectionWrapper>
             }
             {isMobile &&
                 <NavSectionWrapper>
                     <MobileNavSectionContainer>
-                        <div>
-                            <MobileNavTitle style={{color: "#1454FF"}}>{wrapTextWithLang("어려운 주식 투자")}</MobileNavTitle>
-                            <MobileNavTitle style={{paddingBottom: "0.6rem"}}>{wrapTextWithLang("AI 기술과 함께")}</MobileNavTitle>
-                            <MobileNavContent>{wrapTextWithLang("AI 모델 성과 및 수익률 분석\n서비스에서 선정된\n자산별 현황을 확인해보세요")}</MobileNavContent>
-                        </div>
-                        <MobileLogoImage src={stock_character} alt={"image"}/>
+                        {/*<div>*/}
+                            <MobileNavTitle style={{color: "#1454FF"}}>{"어려운 주식 투자"}</MobileNavTitle>
+                            <MobileNavTitle style={{paddingBottom: "0.6rem"}}>{"AI 기술과 함께 알아보세요"}</MobileNavTitle>
+                            <MobileNavContent>{"AI 모델 성과 및 수익률 분석\n서비스에서 선정된 자산별 현황을 확인해보세요"}</MobileNavContent>
+                            <MobileButton onClick={navigateStockList}>AI 성과 분석 확인하기</MobileButton>
+                        {/*</div>*/}
+                        {/*<MobileLogoImage src={stock_character} alt={"image"}/>*/}
                     </MobileNavSectionContainer>
                 </NavSectionWrapper>
             }
